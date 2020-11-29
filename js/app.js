2393214f1c074fde19abe93e6557b4fa498685be
nikkinine9 <nikkinine75@gmail.com>
2020-11-29 15:09:09 -0700

    w11b ajax and apis and json

--- js/app.js
+++ js/app.js
@@ -2,8 +2,21 @@ function getWelcome() {
     var ajaxRequest = new XMLHttpRequest();
     ajaxRequest.onreadystatechange = function() {
         if (ajaxRequest.readyState == 4) {
+            //the request is completed, now check its status
             if (ajaxRequest.status == 200) {
-                document.getElementById("welcome").innerHTML = ajaxRequest.responseText;
+                //turn JSON into object
+                var jsonObj = JSON.parse(ajaxRequest.responseText);
+
+                //get random object from array
+                var randomMessagesArray = jsonObj.randomMessages;
+                var randomIndex = Math.floor(
+                    Math.random() * randomMessagesArray.length
+                );
+                var messageObj = randomMessagesArray[randomIndex];
+
+                //use that object to set content and color
+                document.getElementById("welcome").innerHTML = messageObj.message;
+                document.getElementById("welcome").style.color = messageObj.color;
             } else {
                 console.log("Status error: " + ajaxRequest.status);
             }
@@ -13,7 +26,7 @@ function getWelcome() {
     };
     ajaxRequest.open(
         "GET",
-        "https://happycoding.io/tutorials/javascript/example-ajax-files/simple-welcome.txt"
+        "https://happycoding.io/tutorials/javascript/example-ajax-files/random-welcomes.json"
     );
     ajaxRequest.send();
 }
